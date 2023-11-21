70beeddcc479a196054ce1abc11c64d3b7566138
YasiruRubasinghe <yasirusachintha0810@gmail.com>
2023-11-21 14:39:43 +0530

    update login route

--- controllers/views/login/login.js
+++ controllers/views/login/login.js
@@ -1,7 +1,5 @@
 app.controller('login', function ($scope, $location) {
     $scope.login = function () {
-        // Perform login logic (e.g., validate credentials)
-        // If successful, redirect to the default page
-        $location.path('/default');
+        $location.path('/');
     };
 });
